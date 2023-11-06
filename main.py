07b6bc7ecd34e3528de8c898ae261c7e75909503
suhaanakhan <80097825+suhaanakhan@users.noreply.github.com>
2023-11-06 12:15:07 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-def factorialofnum(n):
-  if (n==0):
-    return 1
-  else:
-    return n*factorialofnum(n-1)
-
-num = int(input("Enter the number : "))
-res = factorialofnum(num)
-print("The Factorial of",num,"is", res)
